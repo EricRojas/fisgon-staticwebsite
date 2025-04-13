e70bfa28ab65cc5923440ed02c834cb3a8b47a04
EricRojas <ericrojasf@hotmail.com>
2025-04-13 17:34:48 -0500

    Add new pages and layout components for Fisgon-Route application
    - Created PageLayout component for consistent layout structure.
    - Developed Soluciones page with features and images related to Fisgon-Route.
    - Implemented Soporte page with placeholder content indicating construction.
    - Added Contacto page with a comprehensive contact form and company information.

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -51,7 +51,7 @@ export default function Example() {
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
           <div className="flex shrink-0 items-center">
               <img
-                alt="Your Company"
+                alt="Fisgon-Route"
                 src={fisgonIcon}
                 className="h-8 w-auto"
               />
@@ -59,7 +59,24 @@ export default function Example() {
           </div>
         </div>
       </div>
-
+      <DisclosurePanel className="sm:hidden">
+        <div className="space-y-1 px-2 pt-2 pb-3">
+          {navigation.map((item) => (
+            <DisclosureButton
+              key={item.name}
+              as="a"
+              href={item.href}
+              aria-current={item.current ? 'page' : undefined}
+              className={classNames(
+                item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                'block rounded-md px-3 py-2 text-base font-medium',
+              )}
+            >
+              {item.name}
+            </DisclosureButton>
+          ))}
+        </div>
+      </DisclosurePanel>
     </Disclosure>
   )
 }
