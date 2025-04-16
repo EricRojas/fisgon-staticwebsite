c8ee3214750301625202ec9e6dc1f03057f6435f
EricRojas <ericrojasf@hotmail.com>
2025-04-15 20:26:30 -0500

    Add Caracteristicas and Soluciones pages with product features and details
    - Implemented Caracteristicas.jsx to showcase features of the Fisgon-Route application, including images and descriptions.
    - Created Soluciones.jsx to present the composition of the basic Fisgon-Route kit, including product images, highlights, and detailed descriptions.

--- src/pages/3.soluciones/Soluciones.jsx
+++ src/pages/3.soluciones/Soluciones.jsx
@@ -11,6 +11,9 @@ import FisgonPCScreen1 from "../../assets/images/FisgonPCScreen_1b.jpg"
 const product = {
   name: 'Kit FisgonRoute',
   name1: 'Kit FisgonRoute1',
+  breadcrumbs: [
+    { id: 1, name: 'Men', href: '#' },
+  ],
   images: [
     {
       src: kitFisgon1,
@@ -47,10 +50,34 @@ const Soluciones = () => {
   return (
     <div className="bg-white">
       <div className="pt-6">
-      <p className="text-3xl text-start tracking-tight text-gray-900">Composición del paquete básico Fisgon-Route</p>
-
-
-
+      <nav aria-label="Breadcrumb">
+          <ol role="list" className="mx-auto flex max-w-2xl items-center space-x-2 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+            {product.breadcrumbs.map((breadcrumb) => (
+              <li key={breadcrumb.id}>
+                <div className="flex items-center">
+                  <a href={breadcrumb.href} className="mr-2 text-sm font-medium text-gray-900">
+                    {breadcrumb.name}
+                  </a>
+                  <svg
+                    fill="currentColor"
+                    width={16}
+                    height={20}
+                    viewBox="0 0 16 20"
+                    aria-hidden="true"
+                    className="h-5 w-4 text-gray-300"
+                  >
+                    <path d="M5.697 4.34L8.98 16.532h1.327L7.025 4.341H5.697z" />
+                  </svg>
+                </div>
+              </li>
+            ))}
+            <li className="text-sm">
+              <a href={product.href} aria-current="page" className="font-medium text-gray-500 hover:text-gray-600">
+                {product.name}
+              </a>
+            </li>
+          </ol>
+        </nav>
         {/* Image gallery */}
         <div className="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-x-8 lg:px-8">
           <img
