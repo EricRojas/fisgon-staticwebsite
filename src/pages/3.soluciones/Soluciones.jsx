9c662988073f3aeaecc4b10c41576676477de963
EricRojas <ericrojasf@hotmail.com>
2025-04-16 06:38:53 -0500

    Cambios en Soluciones

--- src/pages/3.soluciones/Soluciones.jsx
+++ src/pages/3.soluciones/Soluciones.jsx
@@ -76,17 +76,18 @@ const Soluciones = () => {
             className="aspect-4/5 size-full object-cover sm:rounded-lg lg:aspect-auto"
           />
         </div>
-
+      </div>
+      <div className="mx-auto max-w-2xl px-4 pt-10 pb-16 sm:px-6 lg:max-w-7xl lg:px-8">
         {/* Product info */}
         <div className="mx-auto max-w-2xl px-4 pt-10 pb-16 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto_auto_1fr] lg:gap-x-8 lg:px-8 lg:pt-16 lg:pb-24">
           <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
-            <h1 className="text-2xl font-bold tracking-tight text-start text-gray-900 sm:text-3xl">{product.name}</h1>
+            <h2 className="text-2xl font-bold tracking-tight text-start text-gray-900 sm:text-3xl">{product.name}</h2>
           </div>
 
           {/* Options */}
           <div className="mt-4 lg:row-span-3 lg:mt-0">
             <img
-                        alt="Kit Fisgon 10"
+                        alt="Kit Fisgon 3"
                         src={kitFisgon3}
                         className="rounded-lg bg-gray-100"
                       />
@@ -96,8 +97,6 @@ const Soluciones = () => {
           <div className="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 lg:pt-6 lg:pr-8 lg:pb-16">
             {/* Description and details */}
             <div>
-              <h3 className="sr-only">Description</h3>
-
               <div className="space-y-6">
                 <p className="text-base text-gray-900">{product.description}</p>
               </div>
